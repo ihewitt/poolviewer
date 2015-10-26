277ec9d8d9033ec4e2b17b1aede2d0e8289092aa
Andrea Odetti <mariofutire@gmail.com>
2015-10-26 21:05:45 +0000

    Convert LCD Widgets to LineEdit as the former are unreadable.
    Nicer display of times.
    
    Andrea

--- src/editgap.cpp
+++ src/editgap.cpp
@@ -12,6 +12,13 @@ namespace
         average = actualTime / lengths;
     }
 
+    QString formatSecondsToTimeWithMillis(const double value)
+    {
+        QTime time = QTime(0, 0).addMSecs(value * 1000);
+        QString result = time.toString("mm:ss.zzz");
+        return result;
+    }
+
     void displaySet(QTableWidget * grid, int row, const Set & set)
     {
         int lengths;
@@ -28,12 +35,13 @@ namespace
         QTableWidgetItem * actualItem = createTableWidgetItem(QVariant(actual.toString()));
         grid->setItem(row, 2, actualItem);
 
-        QTableWidgetItem * averageItem = createTableWidgetItem(QVariant(average));
+        QTableWidgetItem * averageItem = createTableWidgetItem(QVariant(formatSecondsToTimeWithMillis(average)));
         grid->setItem(row, 3, averageItem);
 
         QTableWidgetItem * speedItem = createTableWidgetItem(QVariant(set.speed));
         grid->setItem(row, 4, speedItem);
     }
+
 }
 
 EditGap::EditGap(QWidget *parent) :
@@ -58,10 +66,10 @@ void EditGap::setOriginalSet(const Set * _set)
 
     // these never change
     displaySet(setGrid, 0, *original);
-    gapTimeLCD->display(gap);
+    gapTimeEdit->setText(formatSecondsToTimeWithMillis(gap));
 
     const double equivalent = gap / average;
-    equivalentLCD->display(equivalent);
+    lengthsEquivalentEdit->setText(QString("%1").arg(equivalent, 0, 'f', 2));
 
     update();
 }
@@ -79,7 +87,7 @@ void EditGap::on_lengthsSpin_valueChanged(int extraLengths)
     if (extraLengths > 0)
     {
         const double average = roundTo8thSecond(gap / extraLengths);
-        averageLCD->display(average);
+        averageTimeEdit->setText(formatSecondsToTimeWithMillis(average));
 
         for (int i = 0; i < extraLengths; ++i)
         {
@@ -102,7 +110,7 @@ void EditGap::on_lengthsSpin_valueChanged(int extraLengths)
     }
     else
     {
-        averageLCD->display(0.0);
+        averageTimeEdit->clear();
     }
     update();
 }
