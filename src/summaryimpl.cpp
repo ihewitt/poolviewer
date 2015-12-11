832829f21260dfc6ca657814eaaabf5db128353c
Ivor Hewitt <ivor.hewitt@gmail.com>
2015-12-11 23:00:36 +0000

    But don't forget to increment the set counter.

--- src/summaryimpl.cpp
+++ src/summaryimpl.cpp
@@ -349,7 +349,10 @@ void SummaryImpl::fillLengths( const Workout& wrk)
     for (it=sets.begin(); it != sets.end(); ++it)
     {
         if (it->times.size() == 0)
+        {
+            set++;
             continue;
+        }
 
         int i;
         for (i = 0; i < it->lens; ++i)
